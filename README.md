1dc4c4a903dc1e277c4edd3e5a46f126fa746930
Nathaniel Nutter <iam@nnutter.com>
2015-05-03 01:15:20 -0500

    skeleton project

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Attempt - It's new $module
+
+# SYNOPSIS
+
+    use Attempt;
+
+# DESCRIPTION
+
+Attempt is ...
+
+# LICENSE
+
+Copyright (C) Nathaniel Nutter.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Nathaniel Nutter <iam@nnutter.com>
---
+++ lib/Attempt.pm
@@ -0,0 +1,39 @@
+package Attempt;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Attempt - It's new $module
+
+=head1 SYNOPSIS
+
+    use Attempt;
+
+=head1 DESCRIPTION
+
+Attempt is ...
+
+=head1 LICENSE
+
+Copyright (C) Nathaniel Nutter.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Nathaniel Nutter E<lt>iam@nnutter.comE<gt>
+
+=cut
+
